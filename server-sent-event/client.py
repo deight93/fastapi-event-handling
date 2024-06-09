e704a30d80b297be49c6a1d0f11e633634e84e15
deight93 <deight8311@gmail.com>
2024-06-09 23:51:20 +0900

    feat: SSE 업데이트

--- server-sent-event/client.py
+++ server-sent-event/client.py
@@ -1,9 +1,7 @@
 import threading
-
 import requests
 
-room_id = "room1"
-
+room_id = "1"
 
 def send_message():
     response = requests.post(
@@ -11,13 +9,13 @@ def send_message():
     )
     print(response.json())
 
-
 def sse_messages():
     response = requests.get(f"http://127.0.0.1:8000/sse/{room_id}", stream=True)
+    client_id = response.headers.get("X-Client-ID")
+    print(f"Client ID: {client_id}")
     for line in response.iter_lines():
         if line:
             print(line.decode("utf-8"))
-
 
 # 메시지 보내기
 send_message_thread = threading.Thread(target=send_message)
--- server-sent-event/main.py
+++ server-sent-event/main.py
@@ -1,28 +1,41 @@
 import asyncio
-
+import uuid
 from fastapi import FastAPI
 from fastapi.responses import StreamingResponse
 
 app = FastAPI()
 
 rooms = {}
-
+clients = {}
 
 @app.post("/send/{room_id}")
 async def send_message(room_id: str, message: str):
     if room_id not in rooms:
         rooms[room_id] = []
     rooms[room_id].append(message)
+    if room_id in clients:
+        for client in clients[room_id].values():
+            await client.put(message)
     return {"status": "Message sent"}
-
 
 @app.get("/sse/{room_id}")
 async def sse_messages(room_id: str):
-    async def event_generator():
-        while True:
-            if room_id in rooms and rooms[room_id]:
-                message = rooms[room_id].pop(0)
+    async def event_generator(client_queue: asyncio.Queue):
+        try:
+            while True:
+                message = await client_queue.get()
                 yield f"data: {message}\n\n"
-            await asyncio.sleep(1)
+        except asyncio.CancelledError:
+            pass
+        finally:
+            # 연결이 끊어지면 큐를 삭제
+            del clients[room_id][client_id]
 
-    return StreamingResponse(event_generator(), media_type="text/event-stream")
+    if room_id not in clients:
+        clients[room_id] = {}
+
+    client_queue = asyncio.Queue()
+    client_id = str(uuid.uuid4())
+    clients[room_id][client_id] = client_queue
+
+    return StreamingResponse(event_generator(client_queue), media_type="text/event-stream", headers={"X-Client-ID": client_id})
